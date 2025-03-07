2581ef3a1b98691fcede34f859ff838f6ed857f0
Hybrid46 <hybrid_46_2@hotmail.com>
2025-03-07 19:51:42 +0100

    Parent component

---
+++ REngine/src/main/java/com/hybrid/rEngine/components/Parent.java
@@ -0,0 +1,41 @@
+package com.hybrid.rEngine.components;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parent extends Component implements Updatable{
+
+    private Transform parentTransform;
+    private final List<Transform> children = new ArrayList<>();
+
+    private Parent() { }
+
+    public Parent(Transform parentTransform){
+        this.parentTransform = parentTransform;
+    }
+
+    @Override
+    public void update() {
+        for (Transform child : children) child.setPosition(parentTransform.getPosition());
+    }
+
+    public boolean addChild(Transform childTransform){
+        boolean containsChild = children.contains(childTransform);
+
+        if (!containsChild) children.add(childTransform);
+
+        return containsChild;
+    }
+
+    public boolean removeChild(Transform childTransform){
+        boolean containsChild = children.contains(childTransform);
+
+        if (containsChild) children.remove(childTransform);
+
+        return containsChild;
+    }
+
+    public int getChildCount (){
+        return children.size();
+    }
+}
