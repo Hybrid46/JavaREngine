b30b1cea33d067ba6f8b9880789fc7fc7e70c39d
Hybrid46 <hybrid_46_2@hotmail.com>
2025-03-15 19:13:17 +0100

    ResourceLoader loads images to image buffer -> optimized memory use

--- REngine/src/main/java/com/hybrid/rEngine/utils/ResourceLoader.java
+++ REngine/src/main/java/com/hybrid/rEngine/utils/ResourceLoader.java
@@ -4,26 +4,35 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 public class ResourceLoader {
+
+    private static final HashMap<String, BufferedImage> images = new HashMap<>();
 
     public static BufferedImage GetSprite(String fileName) {
         //System.out.println("[LoadSave] Loading image -> " + fileName);
 
-        fileName = "/images/" + fileName;
-        BufferedImage img = null;
-        InputStream inputStream = ResourceLoader.class.getResourceAsStream(fileName);
+        if (images.containsKey(fileName)) {
+            return images.get(fileName);
+        }
+
+        String resourcePath = "/images/" + fileName;
+        InputStream inputStream = ResourceLoader.class.getResourceAsStream(resourcePath);
 
         if (inputStream == null) {
-            System.err.println("[LoadSave] Failed to load image -> " + fileName);
-            //return null;
+            System.err.println("[LoadSave] Failed to load image -> " + resourcePath);
+            return null;
         }
 
         try {
-            img = ImageIO.read(inputStream);
-            //System.out.println("[LoadSave] Loaded image -> " + fileName);
+            BufferedImage img = ImageIO.read(inputStream);
+            images.put(fileName, img);  // Use the original fileName as the key
+            System.out.println("[LoadSave] Loaded image -> " + fileName);
+            return img;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         } finally {
             try {
                 inputStream.close();
@@ -31,7 +40,5 @@ public class ResourceLoader {
                 e.printStackTrace();
             }
         }
-
-        return img;
     }
 }
